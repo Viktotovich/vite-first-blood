d572bf9e31bdd0d6707949c7e013cfb386e90581
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2024-12-24 15:44:30 +0400

    Practiced JSX syntax and HTML to JSX conversion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Food } from "./Food";
+import { Converting } from "./ConvertingTest";
 
 function App() {
   return (
     <>
       <p>Here is a list of food I really like</p>
       <Food />
+      <Converting></Converting>
     </>
   );
 }
---
+++ src/ConvertingTest.jsx
@@ -0,0 +1,24 @@
+export function Converting() {
+  return (
+    <>
+      <h1>Test title</h1>
+      <svg>
+        <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+      </svg>
+      <form>
+        <input type="text" />
+      </form>
+    </>
+  );
+}
+
+/*
+    Original HTML:
+<h1>Test title</h1>
+<svg>
+  <circle cx="25" cy="75" r="20" stroke="green" stroke-width="2" />
+</svg>
+<form>
+  <input type="text">
+</form>
+*/
